8d4bf0761f45365ea0e143fd59f467b506efe91f
CastilRonaldo <74065178+CastilRonaldo@users.noreply.github.com>
2020-11-23 20:51:05 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("como les va");
+        System.out.println("como les va");
     }
     
 }
